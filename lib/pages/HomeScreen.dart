31c5436a51df5f4da4d40081b8ec6d59efaafd62
Андрей Морозов <ezforlive2@gmail.com>
2020-07-18 12:53:50 +0300

    i get it!!!!!

--- lib/pages/HomeScreen.dart
+++ lib/pages/HomeScreen.dart
@@ -56,100 +56,205 @@ class HomeScreen extends StatelessWidget {
                       )
                     ),
                   ),
-                Expanded(
-                  flex: 1,
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Row(
-                      children: [
-                        SizedBox(width: 40,),
-                        Text(
-                            "Бесплатные вводные курсы",
-                            style: TextStyle(fontSize: 22, color: Colors.black, fontFamily: 'Roboto_Medium')),
-                      ],
-                    ),
-                  ),
-                ),
+
                 Expanded(
                   flex: 6,
                   child: Container(
-                    margin: EdgeInsets.fromLTRB(0, 0, 0, 20),
+                    margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
                     color: Colors.white,
-                    child: Swiper.children(
+                    child: ListView(
+
                       children: [
-                        Container(
-                            decoration: BoxDecoration(
-                                image: DecorationImage(
-                                    image:  AssetImage(images[0]),
-                                    fit: BoxFit.fill))
-                        ,
-                          child: Stack(
-                            children: [
-                              Container(
-                                width: double.infinity,
-                                child: Row(
-
-                                  children: [
-                                    SizedBox(width: 20),
-                                    Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: [
-                                        SizedBox(height: 50,),
-                                        Text(
-                                            freeTitles[0].toString(),
-                                            style: TextStyle(fontSize: 36, color: Colors.white, fontFamily: 'Roboto_Black')),
-                                        SizedBox(height: 10,),
-                                        Text(
-                                            "Какое-то описание курса",
-                                            style: TextStyle(fontSize: 20, color: Colors.white, fontFamily: 'Roboto_Medium')),
-
-                                      ],
-
-                                    ),
-                                  ],
-                                ),
+                        Column(
+                          children: [
+                            Align(
+                              alignment: Alignment.centerLeft,
+                              child: Row(
+                                children: [
+                                  SizedBox(width: 40,),
+                                  Text(
+                                      "Бесплатные вводные курсы",
+                                      style: TextStyle(fontSize: 22, color: Colors.black, fontFamily: 'Roboto_Medium')),
+                                ],
                               ),
-                              Positioned.fill(
-                                child: Align(
-                                  alignment: Alignment.bottomCenter,
-                                  child: Container(
-                                    margin: EdgeInsets.fromLTRB(20, 0, 20, 20),
-                                    child: SizedBox(
+                            ),
+                            SizedBox(height: 10,),
+                            Container(
+                              width: double.infinity,
+                              height: 400,
+                              child: Swiper.children(
+                                children: [
+                                  Container(
 
-                                      width: double.infinity,
-                                      child: RaisedButton(
-                                        onPressed: (){},
-                                        color: Colors.white,
-                                        shape: RoundedRectangleBorder(
-                                            borderRadius: BorderRadius.circular(10.0),
-                                            side: BorderSide(color: Colors.white)
+                                    decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            image:  AssetImage(images[0]),
+                                            fit: BoxFit.fill))
+                                    ,
+                                    child: Stack(
+                                      children: [
+                                        Container(
+                                          width: double.infinity,
+                                          child: Row(
+
+                                            children: [
+                                              SizedBox(width: 20),
+                                              Column(
+                                                crossAxisAlignment: CrossAxisAlignment.start,
+                                                children: [
+                                                  SizedBox(height: 50,),
+                                                  Text(
+                                                      freeTitles[0].toString(),
+                                                      style: TextStyle(fontSize: 36, color: Colors.white, fontFamily: 'Roboto_Black')),
+                                                  SizedBox(height: 10,),
+                                                  Text(
+                                                      "Какое-то описание курса",
+                                                      style: TextStyle(fontSize: 20, color: Colors.white, fontFamily: 'Roboto_Medium')),
+
+                                                ],
+
+                                              ),
+                                            ],
+                                          ),
                                         ),
-                                        child: Container(
-                                          margin: EdgeInsets.fromLTRB(0, 20,0 , 20),
-                                          child: Text("Начать",
-                                              style: TextStyle(fontSize: 20, color: Colors.black)),
-                                        ),
-                                      ),
+                                        Positioned.fill(
+                                          child: Align(
+                                            alignment: Alignment.bottomCenter,
+                                            child: Container(
+                                              margin: EdgeInsets.fromLTRB(20, 0, 20, 20),
+                                              child: SizedBox(
+
+                                                width: double.infinity,
+                                                child: RaisedButton(
+                                                  onPressed: (){},
+                                                  color: Colors.white,
+                                                  shape: RoundedRectangleBorder(
+                                                      borderRadius: BorderRadius.circular(10.0),
+                                                      side: BorderSide(color: Colors.white)
+                                                  ),
+                                                  child: Container(
+                                                    margin: EdgeInsets.fromLTRB(0, 20,0 , 20),
+                                                    child: Text("Начать",
+                                                        style: TextStyle(fontSize: 20, color: Colors.black)),
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        )
+                                      ],
                                     ),
                                   ),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                        Container(
-                            decoration: BoxDecoration(
-                                image: DecorationImage(
-                                    image:  AssetImage(images[1]),
-                                    fit: BoxFit.fill)))
-                      ],
-                      loop: false,
-                      viewportFraction: 0.8,
-                      scale: 0.9,
-                    )
+                                  Container(
+                                      decoration: BoxDecoration(
+                                          image: DecorationImage(
+                                              image:  AssetImage(images[1]),
+                                              fit: BoxFit.fill)))
+                                ],
+                                loop: false,
+                                viewportFraction: 0.8,
+                                scale: 0.9,
+                              ),
+                            ),
+                            SizedBox(height: 30,),
+                            Align(
+                              alignment: Alignment.centerLeft,
+                              child: Row(
+                                children: [
+                                  SizedBox(width: 40,),
+                                  Text(
+                                      "Платные курсы",
+                                      style: TextStyle(fontSize: 22, color: Colors.black, fontFamily: 'Roboto_Medium')),
+                                ],
+                              ),
+                            ),
+                            SizedBox(height: 10,),
+                            Container(
+                              width: double.infinity,
+                              height: 400,
+                              child: Swiper.children(
+                                children: [
+                                  Container(
+
+                                    decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            image:  AssetImage(images[0]),
+                                            fit: BoxFit.fill))
+                                    ,
+                                    child: Stack(
+                                      children: [
+                                        Container(
+                                          width: double.infinity,
+                                          child: Row(
+
+                                            children: [
+                                              SizedBox(width: 20),
+                                              Column(
+                                                crossAxisAlignment: CrossAxisAlignment.start,
+                                                children: [
+                                                  SizedBox(height: 50,),
+                                                  Text(
+                                                      freeTitles[0].toString(),
+                                                      style: TextStyle(fontSize: 36, color: Colors.white, fontFamily: 'Roboto_Black')),
+                                                  SizedBox(height: 10,),
+                                                  Text(
+                                                      "Какое-то описание курса",
+                                                      style: TextStyle(fontSize: 20, color: Colors.white, fontFamily: 'Roboto_Medium')),
+
+                                                ],
+
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                        Positioned.fill(
+                                          child: Align(
+                                            alignment: Alignment.bottomCenter,
+                                            child: Container(
+                                              margin: EdgeInsets.fromLTRB(20, 0, 20, 20),
+                                              child: SizedBox(
+
+                                                width: double.infinity,
+                                                child: RaisedButton(
+                                                  onPressed: (){},
+                                                  color: Colors.white,
+                                                  shape: RoundedRectangleBorder(
+                                                      borderRadius: BorderRadius.circular(10.0),
+                                                      side: BorderSide(color: Colors.white)
+                                                  ),
+                                                  child: Container(
+                                                    margin: EdgeInsets.fromLTRB(0, 20,0 , 20),
+                                                    child: Text("Начать",
+                                                        style: TextStyle(fontSize: 20, color: Colors.black)),
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                  Container(
+                                      decoration: BoxDecoration(
+                                          image: DecorationImage(
+                                              image:  AssetImage(images[1]),
+                                              fit: BoxFit.fill)))
+                                ],
+                                loop: false,
+                                viewportFraction: 0.8,
+                                scale: 0.9,
+                              ),
+                            ),
+
+                          ],
+                        )
+
+
+                      ],)
                   ),
                 ),
-                
+
               ],
             ),
           )
